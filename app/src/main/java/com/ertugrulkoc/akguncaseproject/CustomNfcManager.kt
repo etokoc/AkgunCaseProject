dc1871229400290a59b92716bcbccc5b6e0e14ad
Ertugrul KOC <etoertugrul.koc@gmail.com>
2023-09-29 15:22:17 +0300

    nfc supported and enabled checking code written

--- app/src/main/java/com/ertugrulkoc/akguncaseproject/CustomNfcManager.kt
+++ app/src/main/java/com/ertugrulkoc/akguncaseproject/CustomNfcManager.kt
@@ -2,25 +2,14 @@ package com.ertugrulkoc.akguncaseproject
 
 import android.app.Activity
 import android.app.PendingIntent
-import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.nfc.tech.IsoDep
-import android.nfc.tech.MifareClassic
-import android.nfc.tech.MifareUltralight
-import android.nfc.tech.Ndef
-import android.nfc.tech.NdefFormatable
-import android.nfc.tech.NfcA
-import android.nfc.tech.NfcB
-import android.nfc.tech.NfcF
-import android.nfc.tech.NfcV
 import com.ertugrulkoc.akguncaseproject.model.NfcCardModel
 
 class CustomNfcManager(private var activity: Activity) {
 
-    private var nfcAdapter: NfcAdapter
+    private var nfcAdapter: NfcAdapter?
     private var pendingIntent: PendingIntent
     private var intent: Intent
 
@@ -32,11 +21,11 @@ class CustomNfcManager(private var activity: Activity) {
     }
 
     fun enableNfcDispatch() {
-        nfcAdapter.enableForegroundDispatch(activity, pendingIntent, null, null)
+        nfcAdapter?.enableForegroundDispatch(activity, pendingIntent, null, null)
     }
 
     fun disableNfcDispatch() {
-        nfcAdapter.disableForegroundDispatch(activity)
+        nfcAdapter?.disableForegroundDispatch(activity)
     }
 
     fun readNfcTag(intent: Intent, result: (NfcCardModel) -> Unit) {
@@ -51,6 +40,14 @@ class CustomNfcManager(private var activity: Activity) {
                 result(nfcCardModel)
             }
         }
+    }
+
+    fun isNfcEnabled(): Boolean {
+        return nfcAdapter?.isEnabled ?: false
+    }
+
+    fun isNfcSupported(): Boolean {
+        return nfcAdapter != null
     }
 
     private fun bytesToHexString(bytes: ByteArray?): String {
--- app/src/main/java/com/ertugrulkoc/akguncaseproject/MainActivity.kt
+++ app/src/main/java/com/ertugrulkoc/akguncaseproject/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.ertugrulkoc.akguncaseproject
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.ertugrulkoc.akguncaseproject.databinding.ActivityMainBinding
 
@@ -18,11 +21,56 @@ class MainActivity : AppCompatActivity() {
         customNfcManager = CustomNfcManager(this)
     }
 
+    /**
+     * Device Nfc Supported Control & Nfc Open-Close Control
+     * */
     override fun onResume() {
         super.onResume()
         if (this::customNfcManager.isInitialized) {
-            customNfcManager.enableNfcDispatch()
+            if (customNfcManager.isNfcSupported()) {
+                if (customNfcManager.isNfcEnabled()) {
+                    customNfcManager.enableNfcDispatch()
+                } else {
+                    openNfcSettings()
+                }
+            } else {
+                // NFC IS NOT SUPPORTED FROM THIS DEVICE
+                openDeviceNotSupportedWindow()
+            }
         }
+    }
+
+    private fun openDeviceNotSupportedWindow() {
+        AlertDialog.Builder(this)
+            .setMessage(getString(R.string.this_device_not_supported_nfc_technology))
+            .setTitle(getString(R.string.warning))
+            .setNeutralButton("Cancel", object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    p0?.dismiss()
+                }
+            })
+    }
+
+    /**
+     * This function has a show dialog. For When Nfc isn't enable, open the nfc settings.
+     */
+    private fun openNfcSettings() {
+        AlertDialog.Builder(this)
+            .setMessage(getString(R.string.nfc_isn_t_enable_please_check_your_nfc_settings))
+            .setTitle(getString(R.string.warning))
+            .setPositiveButton("Open Settings", object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    p0?.dismiss()
+                    val intent = Intent(Settings.ACTION_NFC_SETTINGS)
+                    startActivity(intent)
+                }
+            })
+            .setNegativeButton("Cancel", object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    p0?.dismiss()
+                }
+
+            }).create().show()
     }
 
     override fun onPause() {
